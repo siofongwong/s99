9470785e197256a9d16f500a084c1f8b366a10d4
SioFong Wong <wongx.sio.fong@intel.com>
2015-02-03 18:08:20 -0800

    P09

--- src/main/scala/Practice.scala
+++ src/main/scala/Practice.scala
@@ -23,6 +23,7 @@ object Practice {
     def length(list: List[Any]): Any = {
 
         def length_helper(n: Int, list: List[Any]): Any = list match {
+            case Nil => Nil
             case a :: Nil => n
             case a :: b => length_helper(n + 1, b)
         }
@@ -34,6 +35,38 @@ object Practice {
     }
 
     // P05
-    def reverse(list: List[Any]): Any = Unit
+    def reverse(list: List[Any]): Any = {
+        list.foldLeft(List[Any]())((a, b) => b :: a)
+    }
+
+    // P06
+    def isPalindrome(list: List[Any]): Any = { list == list.reverse }
+    
+    // P07
+    def flatten(l: List[Any]): List[Any] = l flatMap {
+        case ms: List[Any] => flatten(ms)
+        case e => List(e)
+    }
+
+    // P08
+    def compress(l: List[Any]): Any = {
+        l.foldRight(List[Any](l.last))(
+            (a, b) => {
+                if (a != b(0))
+                   a :: b
+                else
+                    b
+            })
+    }
+
+    // P09
+    def pack(l: List[Any]): List[Any] = {
+        val (first, second) = l span { _ == l.head }
+        if (second == Nil) {
+            List(first)
+        } else {
+            first :: pack(second)
+        }
+    }
 }
 
--- src/test/scala/ExampleSpec.scala
+++ src/test/scala/ExampleSpec.scala
@@ -1,5 +1,6 @@
 import org.scalatest._
 import Practice._
+import org.scalatest.Assertions._
 
 class ExampleSpec extends FlatSpec with Matchers {
 
@@ -32,6 +33,33 @@ class ExampleSpec extends FlatSpec with Matchers {
   "P05: reverse(List)" should "reverse a list" in {
     val list = List(1,2,3,4,5)
     reverse(list) should be (List(5,4,3,2,1))
+  }
+
+  "P06: isPalindrome(List)" should "return correct boolean" in {
+    val list = List(1, 2, 3, 2, 1)
+    assert(isPalindrome(list) == true)
+
+    val list2 = List(1, 1, 2, 2 ,3)
+    assert(isPalindrome(list2) == false)
+
+    val list3 = List("a", "b", "c", "c", "b", "a")
+    assert(isPalindrome(list3) == true)
+  }
+
+  "P07: flatten(List)" should "return a flatten list" in {
+    val list = List(List(1, 1), 2, List(3, List(5, 8)))
+    flatten(list) should be (List(1, 1, 2, 3, 5, 8))
+  }
+
+  "P08: compress(List)" should "eliminate consecutive duplicates" in {
+    val list = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    compress(list) should be (List('a, 'b, 'c, 'a, 'd, 'e))
+  }
+
+  "P09: pack(List)" should "pack duplicates of list elements into sublists" in {
+    val list = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    pack(list) should be (List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), 
+      List('a, 'a), List('d), List('e, 'e, 'e, 'e)))
   }
 
 }
